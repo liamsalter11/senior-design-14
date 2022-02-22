f282686aaeab74fb48d07d41c062af48eaa5290b
caf156 <73438961+caf156@users.noreply.github.com>
2022-02-21 22:29:12 -0500

    added liam's receiver class
    should work similarly to liams code but it will print messages and convert the message to string

--- Receiver/Receiver Serial/receiver1.ino
+++ Receiver/Receiver Serial/receiver1.ino
@@ -1,3 +1,13 @@
+// a 11111111 indicates the start and end of transmission
+
+#include "Receiver.h"
+
+# define CLK A1
+# define DIN A0
+
+Receiver receive(CLK, DIN);
+
+
 int state = 0;
 int binaryInput = 7;
 
@@ -7,7 +17,9 @@ byte wholeBIN[100]; //replace number with max number of characters
 
 void setup() 
 {
-  pinMode(binaryInput, INPUT);
+  //pinMode(binaryInput, INPUT);
+  receive.setRGB(A2, A3, A4);
+  Serial.begin(9600);
 
 }
 
@@ -25,14 +37,41 @@ String convertToString(byte bytes[100])
   return convertedMessage;
 }
 
+void printStartUpMessage(void)
+{
+  Serial.print("Welcome! We are waiting for the message to be transmitted to us.\n");
+}
+void printMessageToSerial(String message)
+{
+  Serial.print("Here is the converted message:\n");
+  Serial.print(message);
+  Serial.println("----------\n");
+}
+
+
+
 void getInputBinary(void)
 {
-  int flag = 0;
-
-  while(flag == 0)
+  byte input;
+  for(int i=0; i<sizeof(wholeBIN); i++)
   {
+    input = (byte)receive.ReadByte2();
+    if(input=255)
+      break;
+    else
+      wholeBIN[i] = input;
+  }
+}
+
+void resetSystem(void)
+{
+  for(int i = 0; i<sizeof(wholeBIN);i++)
+  {
+    wholeBIN[i] = 0;
     
   }
+  Serial.flush();
+  Serial.print("\nRestarting..... \n \n");
 }
 
 void loop() 
@@ -41,9 +80,28 @@ void loop()
   switch(state)
   {
     case 0:
+      printStartUpMessage();
+      state=1;
       break;
+    
     case 1:
+      if(receive.ReadByte2()==255)
+        state=2;
+      else
+        state=1;
       break;
+    case 2:
+      getInputBinary();
+      state = 3;
+      break;
+    case 3:
+      printMessageToSerial(convertToString(wholeBIN));
+      state = 4;
+    case 4:
+      resetSystem();
+      state=1;
+      break; 
+      
   }
   
 
